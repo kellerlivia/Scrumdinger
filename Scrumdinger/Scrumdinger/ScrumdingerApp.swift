0f64277f0497105d6788243e2aeab1941fc362cd
kellerlivia <rm94275@fiap.com.br>
2024-11-17 00:04:57 -0300

    feat: MeetingView

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Livia Keller on 16/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
